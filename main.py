83a267c67217cc3bfaf0e86292da6077202dd49f
kaw393939 <keith@webizly.com>
2021-10-14 14:09:06 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+""" content of calculator.py#"""
+
+def inc(x_value):
+    """ Increment Function adds one to the x_value"""
+    return x_value + 1
+def test_answer():
+    """This Tests the function"""
+    assert inc(4) == 5
